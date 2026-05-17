14f000c3334dd2fc08f127360049185839de31fd
ahuvi azulay <ahuviazulay@gmail.com>
2026-05-17 17:22:41 +0300

    feat(order):Prevent the form from going up straight away when the order starts, but only at the opening

--- Project/Controllers/OrdersController.cs
+++ Project/Controllers/OrdersController.cs
@@ -46,22 +46,38 @@ namespace Project.Controllers
             return Ok(order);
         }
 
+        // [HttpPost]
+        //// [Authorize(Roles = "user")]
+        // public async Task<IActionResult> Post([FromBody] OrderDto item)
+        // {
+        //     if (item.StartTime < DateTime.Now.AddMinutes(-5))
+        //     {
+        //         return BadRequest(new { message = "לא ניתן להזמין לתאריך ושעה שעברו" });
+        //     }
+
+        //     var createdOrder = await orderService.Add(item);
+        //     if (createdOrder == null)
+        //         return BadRequest(new { message = "הרכב אינו פנוי בזמנים אלו" });
+
+        //     return Created("", new { message = "ההזמנה בוצעה בהצלחה", data = createdOrder });
+        // }
         [HttpPost]
-       // [Authorize(Roles = "user")]
+        //// [Authorize(Roles = "user")]
         public async Task<IActionResult> Post([FromBody] OrderDto item)
         {
-            if (item.StartTime < DateTime.Now.AddMinutes(-5))
+            // הגדלת הטווח ל-30 דקות אחורה כדי למנוע חסימה של משתמשים איטיים
+            
+            var createdOrder = await orderService.Add(item);
+            if (item.StartTime < DateTime.Now.AddMinutes(-30))
             {
-                return BadRequest(new { message = "לא ניתן להזמין לתאריך ושעה שעברו" });
+                return BadRequest(new { message = "זמן ההתחלה חרג מטווח הסבלנות המותר. אנא עדכן לזמן נוכחי." });
             }
 
-            var createdOrder = await orderService.Add(item);
             if (createdOrder == null)
                 return BadRequest(new { message = "הרכב אינו פנוי בזמנים אלו" });
 
             return Created("", new { message = "ההזמנה בוצעה בהצלחה", data = createdOrder });
         }
-        
 
         [HttpPut("{id}")]
         [Authorize(Roles = "user")]
